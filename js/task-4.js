e073e5e1372e5912a03bf01eff1c090d3b451120
misha <kiritosan08anime@gmail.com>
2025-10-20 02:24:35 +0200

    my seventh js-hw

---
+++ js/task-4.js
@@ -0,0 +1,20 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+
+  const { email, password } = event.currentTarget.elements;
+
+  const userData = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  if (userData.email === "" || userData.password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  console.log(userData); 
+  loginForm.reset(); 
+});
